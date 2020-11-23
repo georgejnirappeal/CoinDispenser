c368c7d202bf115e6012ea0cb0b54d0088040078
George JoseNirappeal <georgejnirappeal@gmail.com>
2020-11-22 21:14:36 -0500

    clean up and fixes

--- src/main/java/com/adp/coindispenser/repository/CoinDispenserDAO.java
+++ src/main/java/com/adp/coindispenser/repository/CoinDispenserDAO.java
@@ -1,6 +1,7 @@
 package com.adp.coindispenser.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -44,23 +45,29 @@ public class CoinDispenserDAO implements ICoinDispenserDAO {
    
 	public void updateDispercerWithProcessedQuantity(Map<Float, Integer> processedCoinsMap) {
 		if (processedCoinsMap != null && !processedCoinsMap.isEmpty()) {
+			try {
 
-			List<SqlParameterSource> namedParameterList = new ArrayList<>();
-
-			for (Map.Entry<Float, Integer> entry : processedCoinsMap.entrySet()) {
-				SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("TYPE", entry.getKey())
-						.addValue("PROCESSED_QUANTITY", entry.getValue());
-
-				namedParameterList.add(namedParameters);
-			}
-			SqlParameterSource[] parameterArray = namedParameterList
-					.toArray(new SqlParameterSource[namedParameterList.size()]);
-
-			int[] itemsProcessed = namedParameterJdbcTemplate.batchUpdate(UPDATE_PROCESSED_COINS_QUERY, parameterArray);
+				List<SqlParameterSource> namedParameterList = new ArrayList<>();
+	
+				for (Map.Entry<Float, Integer> entry : processedCoinsMap.entrySet()) {
+					SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("TYPE", entry.getKey())
+							.addValue("PROCESSED_QUANTITY", entry.getValue());
+	
+					namedParameterList.add(namedParameters);
+				}
+				SqlParameterSource[] parameterArray = namedParameterList
+						.toArray(new SqlParameterSource[namedParameterList.size()]);
+	
+				int[] itemsProcessed = namedParameterJdbcTemplate.batchUpdate(UPDATE_PROCESSED_COINS_QUERY, parameterArray);
+				
+			    if( Arrays.stream(itemsProcessed).filter(result->result!=1).findFirst().isPresent()) {
+			    	LOGGER.warn("updateDispercerWithProcessedQuantity batch processing result is null or empty result: {}", itemsProcessed);
+			    	throw new CDDatabaseException(ErrorCode.CD_DATABAE_100, ErrorType.DATABASE, "Invalid batch processing status");
+			    }
 			
-			if(itemsProcessed == null || ! (itemsProcessed.length > 0)) {
-				 LOGGER.warn("updateDispercerWithProcessedQuantity batch processing result is null or empty result: {}", itemsProcessed);
-	             throw new CDDatabaseException(ErrorCode.CD_DATABAE_100, ErrorType.DATABASE, "Batch not processed");
+			}catch(DataAccessException dae) {
+				 LOGGER.error("getAvailableCoins error while fetching the records ", dae);
+	             throw new CDDatabaseException(ErrorCode.CD_DATABAE_101, ErrorType.DATABASE, "Exception while accessing the data");
 			}
 		}
 	}
--- src/test/java/com/adp/coindispenser/config/DBConfiguration.java
+++ src/test/java/com/adp/coindispenser/config/DBConfiguration.java
@@ -4,18 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 public class DBConfiguration {
 
 	@Bean
-	@Primary
 	@Profile("test")
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("classpath:cd_schema.sql")
